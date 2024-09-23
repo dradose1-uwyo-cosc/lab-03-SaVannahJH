b41c20c53ad10cfc438d2eae7df842fc343ed7ed
SaVannahJH <shussey1@uwyo.edu>
2024-09-23 22:10:29 +0000

    Finished all three sections of Lab 03

---
+++ practice.py
@@ -0,0 +1,14 @@
+Friends= ["Christen", "Hawkins", "SaVannah", "Riley","Ezra","Rylan"]
+print(Friends)
+
+#Delete
+del Friends [2]
+print(Friends)
+
+#ForLoop
+for Friend in Friends
+print(f"{Friends}is on my table")
+
+#Sort Names
+Friends.sort()
+print(Friends)
